7bb106791c3ef60bd8f88379221192f025632187
Flytre <flytre7@gmail.com>
2021-06-19 13:13:19 -0400

    Translucent Cycling Buttons!
    Signed-off-by: Flytre <flytre7@gmail.com>

--- src/main/java/net/flytre/flytre_lib/client/gui/TranslucentButton.java
+++ src/main/java/net/flytre/flytre_lib/client/gui/TranslucentButton.java
@@ -1,13 +1,8 @@
 package net.flytre.flytre_lib.client.gui;
 
-import com.mojang.blaze3d.platform.GlStateManager;
-import com.mojang.blaze3d.systems.RenderSystem;
+import net.flytre.flytre_lib.client.util.RenderUtils;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.widget.ButtonWidget;
-import net.minecraft.client.render.BufferBuilder;
-import net.minecraft.client.render.Tessellator;
-import net.minecraft.client.render.VertexFormat;
-import net.minecraft.client.render.VertexFormats;
 import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.text.Text;
 
@@ -30,46 +25,10 @@ public class TranslucentButton extends ButtonWidget {
                 color = 0x886b6b6b;
             }
 
-            drawRect(x, y, x + width, y + height, color);
+            RenderUtils.drawRect(x, y, x + width, y + height, color);
             drawCenteredText(matrixStack, client.textRenderer, getMessage(), x + width / 2, y + (height - 8) / 2, 0xffffff);
         }
     }
 
 
-    public static void drawRect(int left, int top, int right, int bottom, int color) {
-        if (left < right) {
-            int temp = left;
-            left = right;
-            right = temp;
-        }
-
-        if (top < bottom) {
-            int temp = top;
-            top = bottom;
-            bottom = temp;
-        }
-
-        final float red = (float) (color >> 16 & 255) / 255.0F;
-        final float green = (float) (color >> 8 & 255) / 255.0F;
-        final float blue = (float) (color & 255) / 255.0F;
-        final float alpha = (float) (color >> 24 & 255) / 255.0F;
-
-        final Tessellator tessellator = Tessellator.getInstance();
-        final BufferBuilder buffer = tessellator.getBuffer();
-
-        RenderSystem.enableBlend();
-        RenderSystem.disableTexture();
-        RenderSystem.blendFuncSeparate(GlStateManager.SrcFactor.SRC_ALPHA, GlStateManager.DstFactor.ONE_MINUS_SRC_ALPHA, GlStateManager.SrcFactor.ONE, GlStateManager.DstFactor.ZERO);
-        RenderSystem.setShaderColor(red, green, blue, alpha);
-
-        buffer.begin(VertexFormat.DrawMode.QUADS, VertexFormats.POSITION);
-        buffer.vertex(left, bottom, 0.0D).next();
-        buffer.vertex(right, bottom, 0.0D).next();
-        buffer.vertex(right, top, 0.0D).next();
-        buffer.vertex(left, top, 0.0D).next();
-        tessellator.draw();
-
-        RenderSystem.enableTexture();
-        RenderSystem.disableBlend();
-    }
 }
--- src/main/java/net/flytre/flytre_lib/client/util/RenderUtils.java
+++ src/main/java/net/flytre/flytre_lib/client/util/RenderUtils.java
@@ -1,13 +1,13 @@
 package net.flytre.flytre_lib.client.util;
 
+import com.mojang.blaze3d.platform.GlStateManager;
+import com.mojang.blaze3d.systems.RenderSystem;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 import net.fabricmc.fabric.api.client.render.fluid.v1.FluidRenderHandler;
 import net.fabricmc.fabric.api.client.render.fluid.v1.FluidRenderHandlerRegistry;
 import net.minecraft.client.model.ModelPart;
-import net.minecraft.client.render.RenderLayer;
-import net.minecraft.client.render.VertexConsumer;
-import net.minecraft.client.render.VertexConsumerProvider;
+import net.minecraft.client.render.*;
 import net.minecraft.client.texture.Sprite;
 import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.fluid.Fluid;
@@ -243,4 +243,44 @@ public class RenderUtils {
         matrices.pop();
     }
 
+
+    /**
+     * Draw a rectangle in a GUI
+     */
+    public static void drawRect(int left, int top, int right, int bottom, int color) {
+        if (left < right) {
+            int temp = left;
+            left = right;
+            right = temp;
+        }
+
+        if (top < bottom) {
+            int temp = top;
+            top = bottom;
+            bottom = temp;
+        }
+
+        final float red = (float) (color >> 16 & 255) / 255.0F;
+        final float green = (float) (color >> 8 & 255) / 255.0F;
+        final float blue = (float) (color & 255) / 255.0F;
+        final float alpha = (float) (color >> 24 & 255) / 255.0F;
+
+        final Tessellator tessellator = Tessellator.getInstance();
+        final BufferBuilder buffer = tessellator.getBuffer();
+
+        RenderSystem.enableBlend();
+        RenderSystem.disableTexture();
+        RenderSystem.blendFuncSeparate(GlStateManager.SrcFactor.SRC_ALPHA, GlStateManager.DstFactor.ONE_MINUS_SRC_ALPHA, GlStateManager.SrcFactor.ONE, GlStateManager.DstFactor.ZERO);
+        RenderSystem.setShaderColor(red, green, blue, alpha);
+
+        buffer.begin(VertexFormat.DrawMode.QUADS, VertexFormats.POSITION);
+        buffer.vertex(left, bottom, 0.0D).next();
+        buffer.vertex(right, bottom, 0.0D).next();
+        buffer.vertex(right, top, 0.0D).next();
+        buffer.vertex(left, top, 0.0D).next();
+        tessellator.draw();
+
+        RenderSystem.enableTexture();
+        RenderSystem.disableBlend();
+    }
 }
