03f1bac2fb6a46312187aa6dfa6acf35b9eea013
Flytre <flytre7@gmail.com>
2021-07-11 10:08:07 -0400

    -Made Sets serialize sorted
    Signed-off-by: Flytre <flytre7@gmail.com>

--- src/main/java/net/flytre/flytre_lib/config/GsonHelper.java
+++ src/main/java/net/flytre/flytre_lib/config/GsonHelper.java
@@ -9,8 +9,10 @@ import net.minecraft.entity.attribute.EntityAttribute;
 import net.minecraft.entity.effect.StatusEffect;
 import net.minecraft.fluid.Fluid;
 import net.minecraft.item.Item;
+import net.minecraft.sound.SoundEvent;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
+import net.minecraft.village.VillagerProfession;
 
 import java.lang.reflect.Type;
 import java.util.HashMap;
@@ -32,7 +34,7 @@ public class GsonHelper {
         GSON_BUILDER.setPrettyPrinting();
         GSON_BUILDER.registerTypeAdapter(Identifier.class, IDENTIFIER_SERIALIZER);
         GSON_BUILDER.registerTypeAdapter(new TypeToken<Set<Identifier>>() {
-        }.getType(), new SetDeserializer<>(i -> i));
+        }.getType(), new SetSerializer<>(Function.identity(),Function.identity()));
 
 
         GSON_BUILDER.registerTypeAdapter(EntityType.class, new IdentifierBasedSerializer<EntityType<?>>(Registry.ENTITY_TYPE::get, Registry.ENTITY_TYPE::getId));
@@ -42,6 +44,18 @@ public class GsonHelper {
         GSON_BUILDER.registerTypeAdapter(Enchantment.class, new IdentifierBasedSerializer<>(Registry.ENCHANTMENT::get, Registry.ENCHANTMENT::getId));
         GSON_BUILDER.registerTypeAdapter(Item.class, new IdentifierBasedSerializer<>(Registry.ITEM::get, Registry.ITEM::getId));
         GSON_BUILDER.registerTypeAdapter(EntityAttribute.class, new IdentifierBasedSerializer<>(Registry.ATTRIBUTE::get, Registry.ATTRIBUTE::getId));
+        GSON_BUILDER.registerTypeAdapter(SoundEvent.class, new IdentifierBasedSerializer<>(Registry.SOUND_EVENT::get, Registry.SOUND_EVENT::getId));
+        GSON_BUILDER.registerTypeAdapter(VillagerProfession.class, new IdentifierBasedSerializer<>(Registry.VILLAGER_PROFESSION::get, Registry.VILLAGER_PROFESSION::getId));
+
+        GSON_BUILDER.registerTypeAdapter(new TypeToken<Set<EntityType<?>>>(){}.getType(),new SetSerializer<>(Registry.ENTITY_TYPE::get, Registry.ENTITY_TYPE::getId));
+        GSON_BUILDER.registerTypeAdapter(new TypeToken<Set<Fluid>>(){}.getType(),new SetSerializer<>(Registry.FLUID::get, Registry.FLUID::getId));
+        GSON_BUILDER.registerTypeAdapter(new TypeToken<Set<StatusEffect>>(){}.getType(),new SetSerializer<>(Registry.STATUS_EFFECT::get, Registry.STATUS_EFFECT::getId));
+        GSON_BUILDER.registerTypeAdapter(new TypeToken<Set<Block>>(){}.getType(),new SetSerializer<>(Registry.BLOCK::get, Registry.BLOCK::getId));
+        GSON_BUILDER.registerTypeAdapter(new TypeToken<Set<Enchantment>>(){}.getType(),new SetSerializer<>(Registry.ENCHANTMENT::get, Registry.ENCHANTMENT::getId));
+        GSON_BUILDER.registerTypeAdapter(new TypeToken<Set<Item>>(){}.getType(),new SetSerializer<>(Registry.ITEM::get, Registry.ITEM::getId));
+        GSON_BUILDER.registerTypeAdapter(new TypeToken<Set<EntityAttribute>>(){}.getType(),new SetSerializer<>(Registry.ATTRIBUTE::get, Registry.ATTRIBUTE::getId));
+        GSON_BUILDER.registerTypeAdapter(new TypeToken<Set<SoundEvent>>(){}.getType(),new SetSerializer<>(Registry.SOUND_EVENT::get, Registry.SOUND_EVENT::getId));
+        GSON_BUILDER.registerTypeAdapter(new TypeToken<Set<VillagerProfession>>(){}.getType(),new SetSerializer<>(Registry.VILLAGER_PROFESSION::get, Registry.VILLAGER_PROFESSION::getId));
 
         GSON = GSON_BUILDER.create();
     }
@@ -54,8 +68,6 @@ public class GsonHelper {
         public IdentifierBasedSerializer(Function<Identifier, T> fromId, Function<T, Identifier> toId) {
             this.fromId = fromId;
             this.toId = toId;
-            GSON_BUILDER.registerTypeAdapter(new TypeToken<Set<T>>() {
-            }.getType(), new SetDeserializer<>(fromId));
         }
 
         @Override
@@ -70,12 +82,19 @@ public class GsonHelper {
         }
     }
 
-    public static class SetDeserializer<T> implements JsonDeserializer<Set<T>> {
+    public static class SetSerializer<T> implements JsonSerializer<Set<T>>, JsonDeserializer<Set<T>> {
 
         private final Function<Identifier, T> fromId;
+        private final Function<T, Identifier> toId;
 
-        public SetDeserializer(Function<Identifier, T> fromId) {
+        public SetSerializer(Function<Identifier, T> fromId, Function<T, Identifier> toId) {
+            this.toId = toId;
             this.fromId = fromId;
+        }
+
+        @Override
+        public JsonElement serialize(Set<T> identifiers, Type type, JsonSerializationContext jsonSerializationContext) {
+            return jsonSerializationContext.serialize(identifiers.stream().map(toId::apply).sorted().collect(Collectors.toList()));
         }
 
         @Override
