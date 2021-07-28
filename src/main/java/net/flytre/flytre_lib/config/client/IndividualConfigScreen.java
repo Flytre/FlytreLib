95aa7b1ddcf62e54c95b5fcec03b6645a21990c7
Flytre <flytre7@gmail.com>
2021-07-28 18:36:13 -0400

    Ability to open the config on disk
    Signed-off-by: Flytre <flytre7@gmail.com>

--- src/main/java/net/flytre/flytre_lib/config/client/IndividualConfigScreen.java
+++ src/main/java/net/flytre/flytre_lib/config/client/IndividualConfigScreen.java
@@ -1,11 +1,19 @@
 package net.flytre.flytre_lib.config.client;
 
+import net.fabricmc.loader.api.FabricLoader;
+import net.flytre.flytre_lib.client.gui.TranslucentButton;
 import net.flytre.flytre_lib.config.ConfigHandler;
 import net.flytre.flytre_lib.config.client.list.ConfigListWidget;
 import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.client.util.math.MatrixStack;
+import net.minecraft.text.TranslatableText;
+import net.minecraft.util.Util;
 import org.jetbrains.annotations.Nullable;
 
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -15,9 +23,12 @@ import java.util.List;
  */
 public class IndividualConfigScreen<T> extends GenericConfigScreen {
 
-    private ConfigListWidget list;
     private final ConfigHandler<T> handler;
     private final List<ConfigListWidget.ConfigEntry> entries;
+    private ConfigListWidget list;
+    private TranslucentButton fileOpener;
+    private TranslucentButton helpMac;
+    private TranslucentButton helpWindows;
 
 
     public IndividualConfigScreen(@Nullable Screen parent, ConfigHandler<T> handler) {
@@ -33,6 +44,9 @@ public class IndividualConfigScreen<T> extends GenericConfigScreen {
     public void render(MatrixStack matrices, int mouseX, int mouseY, float delta) {
         super.render(matrices, mouseX, mouseY, delta);
         this.list.render(matrices, mouseX, mouseY, delta);
+        this.fileOpener.render(matrices, mouseX, mouseY, delta);
+        this.helpWindows.render(matrices, mouseX, mouseY, delta);
+        this.helpMac.render(matrices, mouseX, mouseY, delta);
     }
 
     @Override
@@ -51,5 +65,28 @@ public class IndividualConfigScreen<T> extends GenericConfigScreen {
         entries.forEach(i -> list.addConfigEntry(i));
         addSelectableChild(list);
         super.init();
+
+        fileOpener = new TranslucentButton(width / 2 - width / 10, height - 30, width / 5, 20, new TranslatableText("flytre_lib.gui.open_file"), (button) -> {
+            Path location = FabricLoader.getInstance().getConfigDir();
+            Path path = Paths.get(location.toString(), handler.getName() + ".json5");
+            Util.getOperatingSystem().open(path.toFile().toURI());
+        });
+        helpMac = new TranslucentButton(width / 2 - width / 5 - width/5, height - 30, width / 5, 20, new TranslatableText("flytre_lib.gui.help_mac"), (button) -> {
+            try {
+                Util.getOperatingSystem().open(new URI("https://support.apple.com/guide/mac-help/choose-an-app-to-open-a-file-on-mac-mh35597/mac"));
+            } catch (URISyntaxException e) {
+                e.printStackTrace();
+            }
+        });
+        helpWindows = new TranslucentButton(width / 2 + width / 5, height - 30, width / 5, 20, new TranslatableText("flytre_lib.gui.help_windows"), (button) -> {
+            try {
+                Util.getOperatingSystem().open(new URI("https://support.microsoft.com/en-us/windows/change-default-programs-in-windows-10-e5d82cad-17d1-c53b-3505-f10a32e1894d"));
+            } catch (URISyntaxException e) {
+                e.printStackTrace();
+            }
+        });
+        addDrawableChild(fileOpener);
+        addDrawableChild(helpMac);
+        addDrawableChild(helpWindows);
     }
 }
