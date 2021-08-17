03227e16f9f14188b017d02fd2a02acd0ce135d9
Flytre <flytre7@gmail.com>
2021-08-17 07:41:12 -0400

    Added the entity renderer registry
    Signed-off-by: Flytre <flytre7@gmail.com>

---
+++ flytre-lib-base/src/main/java/net/flytre/flytre_lib/api/base/registry/EntityRendererRegistry.java
@@ -0,0 +1,13 @@
+package net.flytre.flytre_lib.api.base.registry;
+
+import net.flytre.flytre_lib.mixin.base.EntityRenderersInvoker;
+import net.minecraft.client.render.entity.EntityRendererFactory;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityType;
+
+public class EntityRendererRegistry {
+
+    public static <T extends Entity> void register(EntityType<? extends T> type, EntityRendererFactory<T> factory) {
+        EntityRenderersInvoker.flytre_lib$register(type, factory);
+    }
+}
