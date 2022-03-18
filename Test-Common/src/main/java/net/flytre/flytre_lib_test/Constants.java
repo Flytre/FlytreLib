48e6b2a7e2c573e708d1fbc24c4c8e8bd385741d
Flytre <flytre7@gmail.com>
2022-03-18 19:49:59 -0400

    -Fixed test files
    Signed-off-by: Flytre <flytre7@gmail.com>

---
+++ Test-Common/src/main/java/net/flytre/flytre_lib_test/Constants.java
@@ -0,0 +1,13 @@
+package net.flytre.flytre_lib_test;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public final class Constants {
+    public static final String MOD_ID = "pipe";
+    public static final String MOD_NAME = "Pipe";
+    public static final Logger LOG = LogManager.getLogger(MOD_NAME);
+
+    private Constants() {
+    }
+}
